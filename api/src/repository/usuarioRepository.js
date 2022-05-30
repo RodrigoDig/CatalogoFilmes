7aa504960fe2c856c79367c86b5dc6fd9f85d704
Rodrigo Gomes Gonçalves <rodrigogomesgoncalves5367@gmail.com>
2022-05-30 11:56:14 -0300

    inicio Funçoes

---
+++ api/src/repository/usuarioRepository.js
@@ -0,0 +1,11 @@
+import {con} from './connection.js'
+
+export function login(email, senha){
+    const comando = 
+                `select id_usuario 		id,
+                    nm_usuario		nome,
+                    ds_email			email
+                    from tb_usuario
+                    where ds_email 		= 'admin@admin.com.br'
+                    and ds_senha			= '1234';`
+}
